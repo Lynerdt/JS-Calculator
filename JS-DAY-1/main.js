0488dd7045eccdb60a7ea0517f389b987f274302
Lynerd <tilleryl10@gmail.com>
2023-10-19 13:25:39 -0400

    math still not working

--- JS-DAY-1/main.js
+++ JS-DAY-1/main.js
@@ -5,20 +5,27 @@ let buttons = document.querySelectorAll('button')
 let string = ''
 
 buttons.forEach(element =>{
-  element.addEventListener('click', (b)=>{
-      if(b.target.innertext == '='){
-        string=String(eval(string))
-        inputBox.value = string;
-      }
-      else{
-        string += b.target.innertext
-        inputBox.value = string
-      }
+element.addEventListener('click', (b)=>{
+  if(b.target.innerText == '='){
+      string = String(eval(string))
+      inputBox.value = string;
+  }
+  else{
+string += b.target.innerText
+inputBox.value = string
 
 
-  })
 
 
+  }
+
+
+
+
+
+
+
+})
 
 
 })
